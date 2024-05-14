d3cbde59bbe3dfe03b53ab1399f68acbdac7f0cb
KiiroElff <albita.10.garrido@gmail.com>
2024-05-14 18:23:45 +0200

    Actualizacion
    asda

---
+++ sign-of-life-2/Assets/Scripts/PuzzleCables.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleCables : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
